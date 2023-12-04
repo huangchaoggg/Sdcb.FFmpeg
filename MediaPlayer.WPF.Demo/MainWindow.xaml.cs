7520f8fcf9a7a6502302aaf265d52b640f974ec5
黄超 <576181574@qq.com>
2023-12-04 15:23:52 +0800

    增加示例文件

---
+++ MediaPlayer.WPF.Demo/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MediaPlayer.MediaFramework;
+
+using Microsoft.Win32;
+
+namespace MediaPlayer.WPF.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Player Player { get; init; } = new Player();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog()==true )
+            {
+                await Player.OpenAsync(openFileDialog.FileName);
+                UrlBox.Text = openFileDialog.FileName;
+                Player.Play();
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(Player.IsPlaying)
+            {
+                Player.Pause();
+            }else
+            {
+                if (Player.Statu == MediaStatus.Stop)
+                {
+                    await Player.RepeatPlayerAsync();
+                }
+                Player.Play();
+            }
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            await Player.Stop();
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(UrlBox.Text, UriKind.Absolute))
+                await Player.OpenAsync(UrlBox.Text);
+            else
+                MessageBox.Show("不是一个有效的URL");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Player.PreviewFramePrev();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Player.PreviewFrameNext();
+        }
+    }
+}
---
+++ MediaPlayer.WPF.Demo/TicketToTimeSpanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MediaPlayer.WPF.Demo
+{
+    public class TicketToTimeSpanConverter : IValueConverter
+    {
+        public static TicketToTimeSpanConverter Current = new TicketToTimeSpanConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is long tick)
+                return TimeSpan.FromMilliseconds(tick).ToString("d\\:hh\\:mm\\:ss");
+            return TimeSpan.Zero;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string ss)
+                return TimeSpan.Parse(ss);
+            return 0;
+        }
+    }
+}
